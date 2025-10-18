a2e8e77e25332fb060bcceafe863a66521020180
Karla Yacolca <karlayacolca@Karlas-MacBook-Pro.local>
2025-10-18 03:06:44 -0500

    style(infra): update bucket backend

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "state-bucket-test-100"
-    key    = "lambda-rds-apigw-infra/terraform2.tfstate"
+    bucket = "bucket"
+    key    = "lambda-rds-apigw-infra/terraform.tfstate"
     region = "us-east-1"
   }
 }
